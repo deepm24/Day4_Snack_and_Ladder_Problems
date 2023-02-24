90f137174878cf81ead84d7426933b31adb76d55
deepm24 <deep60640@gmail.com>
2023-02-25 02:29:22 +0530

    Added: The Player rolls the die to get a number between 1 to 6

--- SnackLadder.java
+++ SnackLadder.java
@@ -1,11 +1,13 @@
 package org.example;
 
-
+import java.util.Random;
 public class SnackLadder {
     public static void main(String[] args)
     {
         System.out.println("** WELCOME TO SNACK AND LADDER GAME **");
         int playerPosition=0;
         System.out.println("player start position :"+playerPosition);
+        int randomCh=(int)Math.floor(Math.random()*10)%6+1;
+        System.out.println("player rolls the die and get a number :"+randomCh);
     }
 }
