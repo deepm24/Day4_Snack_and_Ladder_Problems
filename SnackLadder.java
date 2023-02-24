c4f3cdaf61a05c2540c7e2654e78949efeef1f5d
deepm24 <deep60640@gmail.com>
2023-02-25 02:00:39 +0530

    Added : player at start position 0

--- SnackLadder.java
+++ SnackLadder.java
@@ -1,8 +1,11 @@
 package org.example;
 
-public class Main {
+
+public class SnackLadder {
     public static void main(String[] args)
     {
         System.out.println("** WELCOME TO SNACK AND LADDER GAME **");
+        int playerPosition=0;
+        System.out.println("player start position :"+playerPosition);
     }
 }
